3d9c2ba21fa2a61fe890c3a27c17022462cb6f41
BorjaBG <buenoborja@gmail.com>
2019-03-14 23:55:20 +0100

    Button and TextViews imported on MainActivity.

--- SpanishApp/app/src/main/java/com/example/spanishapp/MainActivity.java
+++ SpanishApp/app/src/main/java/com/example/spanishapp/MainActivity.java
@@ -2,12 +2,26 @@ package com.example.spanishapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button login = (Button) findViewById(R.id.btn_login);
+    TextView email = (TextView) findViewById(R.id.txt_email);
+    TextView password = (TextView) findViewById(R.id.txt_pass);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
